8e86b538db353ef82ebcf93ea67a60d53861f90a
Jiao <jiao.lin@gmail.com>
2018-07-28 18:48:27 -0400

    better comments

--- twic.py
+++ twic.py
@@ -12,6 +12,7 @@ outdir = os.path.abspath(sys.argv[1])
 if not os.path.exists(outdir):
     os.makedirs(outdir)
 
+# download html file
 htmlfile = os.path.join(outdir, 'twic.html')
 if not os.path.exists(htmlfile):
     url = "http://theweekinchess.com/twic"
@@ -33,14 +34,16 @@ tables = soup.find_all('table', class_='results-table')
 assert len(tables) == 1
 table = tables[0]
 
+# all rows with data
 trs = table.find_all('tr', class_='')
 print len(trs), 'records'
 
 for tr in trs:
     tds = tr.find_all('td')
+    # first cell is index
     ind = tds[0].text
     sys.stdout.write("%s: " % ind); sys.stdout.flush()
-    # find pgn link
+    # find pgn link in one of the cells
     found = False
     for td in tds[1:]:
         if td.text == 'PGN': 
@@ -62,7 +65,7 @@ for tr in trs:
     zip_ref.extractall(outdir)
     zip_ref.close()
     os.remove(fn)
-    #
+    # check output
     pgnfile = 'twic%s.pgn' % ind
     assert os.path.exists(os.path.join(outdir, pgnfile))
     sys.stdout.write("\n"); sys.stdout.flush()
